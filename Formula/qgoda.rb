7c2ab78081a8129b2c66e7a951c9c4b6aaef785d
Guido Flohr <guido.flohr@cantanea.com>
2023-07-13 17:57:09 +0300

    skeleton for Qgoda

---
+++ Formula/qgoda.rb
@@ -0,0 +1,63 @@
+class Qgoda < Formula
+	desc "Static site generator with built-in multi-language support"
+	homepage "https://www.qgoda.net"
+	url "file:///Users/iknowmyname/perl/qgoda/Qgoda-v0.10.0.tar.gz"
+	sha256 "6a6d896a5e1672a7981503a469b032b9ed5b31ef7ee8be26f564b0e570635483"
+	license "GPL-3.0-or-later"
+
+	depends_on "cpanminus" => :build
+	depends_on "node" => :build
+	depends_on "perl"
+
+	def install
+		# ENV.deparallelize	# if your formula fails when building in parallel
+		ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+		ENV.prepend_path "PERL5LIB", libexec/"lib"
+
+		# Until Mac-FSEvents is released again.
+		on_macos do
+			system "git", "clone", "https://github.com/skaji/Mac-FSEvents"
+			chdir "Mac-FSEvents" do
+				system "git", "reset", "--hard", "4215be367e2dedcc55aa5fc413dc0291ab697604"
+				system "perl", "Makefile.PL",
+					"INSTALL_BASE=#{libexec}",
+					"INSTALLSITEMAN1DIR=#{man1}",
+					"INSTALLSITEMAN3DIR=#{man3}"
+				system "make"
+				system "make", "install"
+			end
+			system "rm", "-rf", "Mac-FSEvents"
+		end
+
+		# The HEAD of the main branch contains is okay here.
+		system "git", "clone", "https://github.com/gflohr/JavaScript-Duktape-XS.git"
+		chdir "JavaScript-Duktape-XS" do
+			system "perl", "Makefile.PL",
+				"INSTALL_BASE=#{libexec}",
+				"INSTALLSITEMAN1DIR=#{man1}",
+				"INSTALLSITEMAN3DIR=#{man3}"
+			system "make"
+			system "make", "install"
+		end
+		system "rm", "-rf", "JavaScript-Duktape-XS"
+
+		ENV["QGODA_PACKAGE_MANAGER"] = Formula["npm"].libexec/"bin/npm"
+		system "cpanm", "--local-lib", "#{libexec}", "--notest", "--installdeps", "."
+
+		system "perl", "Makefile.PL",
+			"INSTALL_BASE=#{libexec}",
+			"INSTALLSITEMAN1DIR=#{man1}",
+			"INSTALLSITEMAN3DIR=#{man3}"
+		system "make"
+		system "make", "install"
+
+		bin.env_script_all_files(libexec/"bin", PERL5LIB: ENV["PERL5LIB"])
+	end
+
+	test do
+		(testpath/"index.html").write("<h1>Hello, world!</h1")
+		system "#{bin}/program", "--quiet", "build"
+		assert_path_exists testpath/"_site/index.html"
+		assert_path_exists testpath/"_timestamp"
+	end
+end
